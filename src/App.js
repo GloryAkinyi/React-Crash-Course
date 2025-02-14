42f7b00f2e9db2c2903f1fd197d67e57140a8e37
gloryakinyi <akinyiglory2@gmail.com>
2025-02-14 03:31:33 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
+
+import { ProductCard } from './components/ProductCard';
+
 import './App.css';
-
-
-
-function ProductCard(){
-  return(
-
-    <article>
-      <h2>iPhone 15 Pro</h2>
-      <p>To use an iPhone, you'll need an Apple ID, internet access, and enough storage space. You can also sync your iPhone to a computer using iTunes or the Finder. </p>
-      <img src='/images/iphone.jpeg' width="500px" heigh="300px"/>
-
-
-      <h2>Specifications</h2>
-      <p>Dimensions	146.7 x 71.5 x 7.8 mm (5.78 x 2.81 x 0.31 in)</p>
-      <p>Weight	172 g (6.07 oz)</p>
-      <p>Splash, Water, and Dust Resistant 3 Rated IP68 (maximum depth of 6 meters up to 30 minutes) under IEC standard 60529.</p>
-      <p>Video Recording. 4K Dolby Vision video recording at 24 fps, 25 fps, 30 fps, or 60 fps.</p>
-
-
-
-    </article>
-  )
-
-
-}
-
 
 
 function App() {
---
+++ src/components/ProductCard.js
@@ -0,0 +1,86 @@
+
+export function ProductCard(){
+
+    const product = {
+
+        imgSrc : "/images/iphone3.jpeg",
+        title : "iPhone 15 Pro",
+        specifications : [
+            "Dimensions	146.7 x 71.5 x 7.8 mm (5.78 x 2.81 x 0.31 in)",
+            "Weight	172 g (6.07 oz)",
+            "Splash, Water, and Dust Resistant 3 Rated IP68 (maximum depth of 6 meters up to 30 minutes) under IEC standard 60529.",
+            "Video Recording. 4K Dolby Vision video recording at 24 fps, 25 fps, 30 fps, or 60 fps."
+
+        ],
+        price : "8900"
+        
+
+    }
+
+
+
+
+    return(
+  
+      <article>
+        <h2>{product.title}</h2>
+        <p>To use an iPhone, you'll need an Apple ID, internet access, and enough storage space. You can also sync your iPhone to a computer using iTunes or the Finder. </p>
+        <img src={product.imgSrc}/>
+  
+  
+        <h2>Specifications</h2>
+  
+        <ul>
+          <li>{product.specifications[0]}</li>
+          <li>{product.specifications[1]}</li>
+          <li>{product.specifications[2]}</li>
+          <li>{product.specifications[3]}</li>
+        </ul>
+  
+  
+        <button>Buy from ${product.price}</button>
+  
+  
+  
+  
+       <div id="main">
+  
+       <div className="sub-division1">
+
+
+        <img src="/images/iphone.jpeg"/>
+        
+        <h2>iPhone 14</h2>
+        <p>Includes crash detection that calls emergency services if it senses a car accident </p>
+        <button>See More</button>
+       </div>
+  
+  
+       <div className="sub-division1">
+        
+        <h2>iPhone 14</h2>
+        <p>Includes crash detection that calls emergency services if it senses a car accident </p>
+        <button>See More</button>
+       </div>
+  
+  
+       <div className="sub-division1">
+        
+        <h2>iPhone 14</h2>
+        <p>Includes crash detection that calls emergency services if it senses a car accident </p>
+        <button>See More</button>
+       </div>
+  
+  
+       </div>
+  
+      </article>
+  
+  
+  
+  
+    )
+  
+  
+  }
+  
