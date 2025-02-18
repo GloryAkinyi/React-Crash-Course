7a90f07c93373151ef40d60c8861b499a8403109
gloryakinyi <akinyiglory2@gmail.com>
2025-02-18 16:52:32 +0300

    another commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header(){
 
     return(
-        <h1>SnapCart : Shop With Us!</h1>
+        <h1 style={{textAlign : "center"}}>SnapCart : Shop With Us!</h1>
     )
 
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,8 +7,57 @@ export default function Contact(){
 
         <div>
 
-            <Header/>
-            
+            <marquee><Header/></marquee>
+
+{/* Form */}
+
+           
+           <div class = "container">
+
+
+            <h3 class="text-center">Create An Account With Us!</h3>
+
+            <form >
+
+                <label htmlFor="fname" class="form-label">FirstName</label>
+                <input type="text" class="form-control" placeholder="Enter firtname" required></input>
+
+
+                <label htmlFor="lname" class="form-label">LastName</label>
+                <input type="text" class="form-control" placeholder="Enter lastname" required></input>
+
+
+                <label htmlFor="email" class="form-label">Email Address</label>
+                <input type="email" class="form-control" placeholder="abc@gmail.com" required></input>
+
+
+
+
+                <label htmlFor="password" class="form-label">Password</label>
+                <input type="password" class="form-control" placeholder="abc@gmail.com" required></input>
+
+
+                <label htmlFor="nationality" class="form-label">Nationality :</label>
+                <select name="" id="nationality" class="form-select">
+                      <option value="choose" disabled="disabled">--Choose nationality--</option>
+                      <option value="kenyan">Kenyan</option>
+                      <option value="american">American</option>
+                      <option value="tanzanian">Tanzanian</option>
+                </select>
+
+
+                <div class="d-grid mt-5">
+                    <button class="btn btn-primary">Register</button>
+                </div>
+
+
+            </form>
+
+
+           </div>
+
+
+           
 
 
 
